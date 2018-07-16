19f4e3305e6a56fad5e97f38041479eae8c220a9
Raphael Ludwig <raphael.ludwig@happywetter.at>
2018-07-16 15:39:38 +0200

    [Fix] heatmap color ramp while rendering into file

--- src/main/scala/his/util/HeatmapGenerator.scala
+++ src/main/scala/his/util/HeatmapGenerator.scala
@@ -36,21 +36,6 @@ class HeatmapGenerator(keymap: Seq[Node], keys: TrieMap[Int, Long], startColor: 
 
   def transform(engine: WebEngine): Unit = {
     keys.foreach { case (keyCode, count) =>
-      var p = count.asInstanceOf[Double] / max
-      var s = startColor
-      var c = endColor
-      if (p <= RAMP_VALUE_1 && p >= RAMP_VALUE_2) {
-        c = Color.Orange
-        p /= RAMP_VALUE_1
-        s = Color.Green
-      } else if (p <= RAMP_VALUE_2) {
-        c = Color.Green
-        p /= RAMP_VALUE_2
-      } else {
-        s = Color.Orange
-      }
-
-      val color = s.interpolate(c, p)
       val elem = engine.document.getElementById(s"0x${keyCode.toHexString.toUpperCase}")
 
       if (elem == null) {
@@ -59,9 +44,28 @@ class HeatmapGenerator(keymap: Seq[Node], keys: TrieMap[Int, Long], startColor: 
           deadKeys += keyCode
         }
       } else {
-        elem.setAttribute("style", s"fill:${color.toRGB};fill-opacity:1;stroke:#202326;stroke-width:0")
+        elem.setAttribute("style", s"fill:${color(keyCode, count).toRGB};fill-opacity:1;stroke:#202326;stroke-width:0")
       }
     }
+  }
+
+  private def color(implicit keyCode: Int, count: Long): Color = {
+    var p = count.asInstanceOf[Double] / max
+    var s = startColor
+    var c = endColor
+
+    if (p <= RAMP_VALUE_1 && p >= RAMP_VALUE_2) {
+      c = Color.Orange
+      p /= RAMP_VALUE_1
+      s = Color.Green
+    } else if (p <= RAMP_VALUE_2) {
+      c = Color.Green
+      p /= RAMP_VALUE_2
+    } else {
+      s = Color.Orange
+    }
+
+    s.interpolate(c, p)
   }
 
   private def max: Long = if(keys.nonEmpty) keys.maxBy(_._2)._2 else 1L
@@ -85,15 +89,13 @@ class HeatmapGenerator(keymap: Seq[Node], keys: TrieMap[Int, Long], startColor: 
     override def transform(n: Node): Seq[Node] = {
       Try(Integer.decode(n \ "@id")).fold(
         (_) => n,
-        (id) => {
-          keys.get(id).fold(n)(count => {
-            val color = startColor.interpolate(endColor, count.asInstanceOf[Double] / max)
-
+        (keyCode) => {
+          keys.get(keyCode).fold(n)(count => {
             n.asInstanceOf[Elem].copy(
               attributes =
                 for (attr <- n.attributes) yield attr match {
                   case attr@Attribute("style", _, _) =>
-                    attr.copyData(value=s"fill:${color.toRGB};fill-opacity:1;stroke:#202326;stroke-width:0")
+                    attr.copyData(value=s"fill:${color(keyCode.toInt, count).toRGB};fill-opacity:1;stroke:#202326;stroke-width:0")
                   case other => other
                 }
             )
